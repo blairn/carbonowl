2480bc90ee8c47697fe873796add902b103598e1
Blair Nilsson <blair.nilsson@gmail.com>
2021-03-16 00:48:02 +1300

    isn't vercel meant to handle nullish by now?

--- src/routes/api/[db]/[collection].js
+++ src/routes/api/[db]/[collection].js
@@ -4,7 +4,7 @@ import {streamResults} from './_streaming.mjs'
 export async function post(req, res, next) {
 	const db = 'overwatch'
   const {collection } = req.params
-  let query = req.body ?? {}
+  let query = req.body || {}
   const q = deepMap((query instanceof Array)?[...query]:[{$match:query}])
   const cursor = await mongo.db(db).collection(collection).aggregate(q).stream()
   streamResults(req,res,cursor)
